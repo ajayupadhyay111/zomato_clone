0436e92adc3b037c2f24f30a8d80390a061cd54b
ajayupadhyay111 <upadhyayajay156@gamil.com>
2026-02-16 21:52:39 +0530

    feat(auth): initialize authentication service with user login functionality
    - Added package.json for auth service with dependencies and scripts.
    - Implemented MongoDB connection in db.ts.
    - Created user model with Mongoose in User.ts.
    - Developed login controller to handle user authentication in auth.ts.
    - Added TryCatch middleware for error handling in async routes.
    - Set up Express server and routes for authentication in index.ts and auth.ts.
    - Configured TypeScript settings in tsconfig.json.

---
+++ services/auth/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js'
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/auth",authRoutes)
+
+app.get('/', (req, res) => {
+  res.send('Hello from Auth Service!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Auth Service is running on port ${PORT}`);
+});
---
+++ services/auth/src/model/User.ts
@@ -0,0 +1,19 @@
+import mongoose ,{Document,Schema} from 'mongoose';
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    image: string;
+    role: string;
+}
+
+const schema:Schema<IUser> = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    image: { type: String, required: false },
+    role: { type: String, default: null }
+},{
+    timestamps: true
+})
+
+export const User = mongoose.model<IUser>('User', schema);
